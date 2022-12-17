30ecadb7d56444956ead1b928a26fb5402dddb03
AvetisDN <avetisdn@gmail.com>
2022-12-17 13:24:25 +0300

    route transitions

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
+
+router.beforeEach((to, from, next) => {
+  console.log(to);
+  next();
+});
+
+const transitionName = ref("");
 </script>
 
 <template>
-  <div class="w-full  h-screen relative ">
-   
-<router-view v-slot="{Component, route}">
-<transition :name="route.meta.transitionName" mode="default">
-  <component :is="Component" />
-  </transition>
-</router-view>
+  <div class="w-full h-screen relative">
+    <router-view v-slot="{ Component, route }">
+      <transition :name="route.meta.transitionName" mode="default">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
-
-
-
--- src/router/index.js
+++ src/router/index.js
@@ -1,52 +1,57 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home,
-        meta: {
-            transitionName: 'slide-right'
-        }
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: {
+      transitionName: "slide-right",
+      order: 0,
     },
-    {
-        path: '/about',
-        name: 'About',
-        component: () => import('../views/About.vue'),
-        meta: {
-            transitionName: 'slide-right'
-        }
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () => import("../views/About.vue"),
+    meta: {
+      transitionName: "slide-right",
+      order: 1,
     },
-    {
-        path: '/planet',
-        name: 'Planet',
-        component: () => import('../views/Planet.vue'),
-        meta: {
-            transitionName: 'slide-left'
-        }
+  },
+  {
+    path: "/planet",
+    name: "Planet",
+    component: () => import("../views/Planet.vue"),
+    meta: {
+      transitionName: "slide-left",
+      order: 2,
     },
-    {
-        path: '/contact',
-        name: 'Contact',
-        component: () => import('../views/Contact.vue'),
-        meta: {
-            transitionName: 'slide-left'
-        }
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: () => import("../views/Contact.vue"),
+    meta: {
+      transitionName: "slide-left",
+      order: 3,
     },
-    {
-        path: '/card',
-        name: 'Card',
-        component: () => import('../views/Card.vue'),
-        meta: {
-            transitionName: 'slide-left'
-        }
+  },
+  {
+    path: "/card",
+    name: "Card",
+    component: () => import("../views/Card.vue"),
+    meta: {
+      transitionName: "slide-left",
+      order: 4,
     },
-]
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-})
+  history: createWebHistory(),
+  routes,
+});
 
 export default router;
